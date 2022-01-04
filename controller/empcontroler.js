81a3aea128ee5f2a8f18470a303bfb36bb7e1376
Avinashparakhiya <90279064+Avinashparakhiya@users.noreply.github.com>
2022-01-04 14:09:57 +0530

    login page

--- controller/empcontroler.js
+++ controller/empcontroler.js
@@ -1,132 +1,135 @@
-const response = require('express')
-const Employee=require('../models/employee')
-//const joi=require('joi')
-//const Joi = require('joi')
+const response = require('express');
+const Employee = require('../models/employee');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const index=(req,res,next)=>
-{
-Employee.find().then(response=>
-    {
-        res.json(
-            {
-             response   
-            }
-        )
+const index = (req, res, next) => {
+  Employee.find()
+    .then((response) => {
+      res.json({
+        response,
+      });
     })
-    .catch(error=>
-        {
-            res.json(
-                {
-                    message:'An Error Occured!'
-                }
-            )
-        })
-}
+    .catch((error) => {
+      res.json({
+        message: 'An Error Occured!',
+      });
+    });
+};
 
-const show=(req,res,next)=>
-{
-    let employeeID=req.body.employeeID
-    Employee.findById(employeeID)
-    .then(response=>
-        {
-            res.json(
-                {
-                    response
-                }
-            )
-        }).catch(error=>
-            {
-                res.json({
-                    message:'An error occured'
-                })
-            })
-}
-
-const store=(req,res,next)=>
-{
-    let employee=new Employee(
-        {
-            name:req.body.name,
-            age:req.body.age,
-            mobileno:req.body.mobileno,
-            email:req.body.email,
-            password:req.body.password
-
-        }
-    )
-
-    .then(response=>
-        {
-            res.json({message:'Employee Add Succesfully'})
-        })
-        .catch(error=>
-            {
-                res.json({
-                    message:'An Error occured'
-                })
-            })
-}
-
-
-const update=(req,res,next)=>
-{
-    let employeeID=req.body.employeeID
-
-    let updateData=
-    {
-         name:req.body.name,
-        age:req.body.age,
-        mobileno:req.body.mobileno,
-        email:req.body.email,
-        password:req.body.password
-
-    }
-
-    Employee.findByIdAndUpdate(employeeID,{$set:updateData})
-    .then(()=>
-    {
-        res.json(
-            {
-                message:'Employee updated Successfully'
-            }
-        )
+const show = (req, res, next) => {
+  let employeeID = req.body.employeeID;
+  Employee.findById(employeeID)
+    .then((response) => {
+      res.json({
+        response,
+      });
     })
-    .catch(error=>{
-        res.json({
-            message:'An error Occured'
-        })
-    })
-}
+    .catch((error) => {
+      res.json({
+        message: 'An error occured',
+      });
+    });
+};
 
-const destroy=(req,res,next)=>
-{
-    let employeeID=req.body.employeeID
-    Employee.findByIdAndRemove(employeeID)
-    .then(()=>
-    {
-       req.json({
-           message:'Employee De;eted Succesully'
-       }) 
+const store = async (req, res, next) => {
+  let hasPassword = await bcrypt.hash(req.body.password, 10);
+  let employee = new Employee({
+    name: req.body.name,
+    age: req.body.age,
+    mobileno: req.body.mobileno,
+    email: req.body.email,
+    password: hasPassword,
+  });
+  console.log(employee);
+  employee
+    .save()
+    .then((response) => {
+      res.json({ message: 'Employee Add Succesfully' });
     })
-    .catch(error=>
-        {
-            req.json(
-                {
-                    message:'An Error Occured'
-                }
-            )
-        })
-}
+    .catch((error) => {
+      res.json({
+        message: 'An Error occured',
+      });
+    });
+};
+
+const update = (req, res, next) => {
+  let employeeID = req.body.employeeID;
+
+  let updateData = {
+    name: req.body.name,
+    age: req.body.age,
+    mobileno: req.body.mobileno,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  Employee.findByIdAndUpdate(employeeID, { $set: updateData })
+    .then(() => {
+      res.json({
+        message: 'Employee updated Successfully',
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: 'An error Occured',
+      });
+    });
+};
+
+const destroy = (req, res, next) => {
+  let employeeID = req.body.employeeID;
+  Employee.findByIdAndRemove(employeeID)
+    .then(() => {
+      req.json({
+        message: 'Employee Deleted Succesully',
+      });
+    })
+    .catch((error) => {
+      req.json({
+        message: 'An Error Occured',
+      });
+    });
+};
 
 const login = (req, res, next) => {
   var email = req.body.email;
   var password = req.body.password;
-  Employee.find({ email: email })
+  Employee.find({ email: req.body.email })
     .exec()
-    .then((email) => {
-      res.status(201).json({
-        message: 'user found',
-      });
+    .then((user) => {
+      if (user.length < 1) {
+        res.status(404).json({
+          message: 'Auth Failed',
+        });
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (!result) {
+            res.status(404).json({
+              message: 'Auth Failed',
+            });
+          }
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: user[0].email,
+              },
+              'this is dummy key',
+              {
+                expiresIn: '24h',
+              }
+            );
+            res.status(200).json({
+              message: 'you are log in',
+            });
+          } else {
+            res.status(404).json({
+              message: 'Auth Failed',
+            });
+          }
+        });
+      }
     })
     .catch((err) => {
       res.json({
@@ -135,5 +138,4 @@ const login = (req, res, next) => {
     });
 };
 
-module.exports={index,show,store,update,destroy}
-
+module.exports = { index, show, store, update, destroy, login };
